43a45d4fda4a01eece653bb07d7678b14c14ec9c
Ahmed Ez zaouia <ahmez-za@e1r12p1.1337.ma>
2022-08-25 05:21:12 +0100

    organize cd add more functions

--- execution/ft_cd_cmd.c
+++ execution/ft_cd_cmd.c
@@ -6,7 +6,7 @@
 /*   By: ahmez-za <ahmez-za@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 02:33:37 by ahmez-za          #+#    #+#             */
-/*   Updated: 2022/08/24 05:17:35 by ahmez-za         ###   ########.fr       */
+/*   Updated: 2022/08/25 05:19:23 by ahmez-za         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,104 +33,90 @@ int     get_oldpwd_index()
     return (i);
 }
 
-int get_env_list_size()
-{
-    int i;
 
-    i = 0;
-    while (g_data.env_list[i])
-        i++;
-    return (i);
+void    go_to_home(char *s)
+{
+    char *item;
+    item = NULL;
+    if (chdir(ft_get_env("HOME")) == -1)
+        perror("Minishell :");
+    else
+    {
+
+        /* TODO: update The pwd on case one arg (cd) */
+        // free(item);
+        item = g_data.env_list[get_pwd_index()];
+        s = getcwd(NULL, 0);
+        g_data.env_list[get_pwd_index()] = ft_strjoin(ft_strdup("PWD="), s);
+        printf("s == %s\n",s);
+        free(item);
+    }
+}
+
+void    update_old_Pwd(char *pwd)
+{
+    int oldpwdindex;
+    char *item;
+    
+    oldpwdindex = get_oldpwd_index();
+
+    if (g_data.env_list[oldpwdindex] == NULL)
+    {
+        g_data.env_list = ft_realloc_er(g_data.env_list, sizeof(char *), g_data.size_env_list + 1);
+        g_data.env_list[g_data.size_env_list] = ft_strjoin(ft_strdup("OLD"), pwd);
+        g_data.env_list[g_data.size_env_list + 1] = NULL;
+    }
+    else
+    {
+        item = g_data.env_list[oldpwdindex]; 
+        g_data.env_list[get_oldpwd_index()] = ft_strjoin(ft_strdup("OLD"), pwd);
+        free(item);
+    }
+}
+
+void    change_dir(t_AST *cmd_strc, char *s, char *str_join, char *pwd)
+{
+    char *item;
+
+    item = NULL;
+    s = getcwd(NULL, 0);
+    if (s == NULL)
+        {
+            item =  g_data.env_list[get_pwd_index()];
+            printf("Minishell : error retrieving current directory: getcwd: cannot access parent directories: No such file or directory\n");
+            str_join = ft_strjoin(ft_strdup("/"), cmd_strc->args[1]);
+            g_data.env_list[get_pwd_index()] = ft_strjoin(ft_strdup(pwd),str_join);
+            free(item);
+            free(str_join);
+        }
+    else
+        {   
+            item = g_data.env_list[get_pwd_index()];
+            g_data.env_list[get_pwd_index()] = ft_strjoin(ft_strdup("PWD="), s);
+            free(item);
+        }
 }
 
 void    ft_cd_cmd(t_AST *cmd_strc)
 {
-    int i;
-    int len;
     char *str_join;
     char *pwd;
     char *s;
     char *item;
 
-
     s = NULL;
     item = NULL;
-
-
-    len = get_env_list_size();
+    str_join = NULL;
     pwd = g_data.env_list[get_pwd_index()];
-    int oldpwdindex = get_oldpwd_index();
-    printf("oldpwd index === %d\n",oldpwdindex);
-    printf("oldpwd == %s\n", g_data.env_list[get_oldpwd_index()]);
-    if (g_data.env_list[oldpwdindex] == NULL)
-    {
-        g_data.env_list = ft_realloc_er(g_data.env_list, sizeof(char *), len + 1);
-        g_data.env_list[len] = ft_strjoin(ft_strdup("OLD"), pwd);
-        g_data.env_list[len + 1] = NULL;
-    }
-    else
-    {
-        // ft_sanitize here 
-        item = g_data.env_list[oldpwdindex]; 
-        g_data.env_list[get_oldpwd_index()] = ft_strjoin(ft_strdup("OLD"), pwd);
-        free(item);
-    }
-    i = 1;
-    while ((i < cmd_strc->size_args) && (cmd_strc->args[i][0] == 45))
-        i++;
-
-    if (cmd_strc->args[i] == NULL && i != 1)
-        return ;
+    update_old_Pwd(pwd);
     if (cmd_strc->size_args == 1)
-    {
-
-        if (chdir(getenv("HOME")) == -1)
-            perror("Minishell :");
-        else
-        {
-
-            /* TODO: update The pwd on case one arg (cd) */
-            // free(item);
-            item = g_data.env_list[get_pwd_index()];
-            s = getcwd(NULL, 0);
-            g_data.env_list[get_pwd_index()] = ft_strjoin(ft_strdup("PWD="), s);
-            free(item);
-        }
-    }
+        go_to_home(s);
     else
     {
-        
-
-        if (chdir(cmd_strc->args[i]) == -1)
+        if (chdir(cmd_strc->args[1]) == -1)
             perror("Minishell :");
         else
-        {
-            // item = g_data.env_list[get_pwd_index()];
-            s = getcwd(NULL, 0);
-            printf("pwd == %s\n",pwd);
-            printf("cwd = %s\n",s);
-            if (s == NULL)
-            {
-                item =  g_data.env_list[get_pwd_index()];
-                printf("Minishell : error retrieving current directory: getcwd: cannot access parent directories: No such file or directory\n");
-                str_join = ft_strjoin(ft_strdup("/"), cmd_strc->args[i]);
-                g_data.env_list[get_pwd_index()] = ft_strjoin(ft_strdup(pwd),str_join);
-                free(item);
-                free(str_join);
-            }
-            else
-            {   
-                // printf("**************** cwd else ********\n");
-                item = g_data.env_list[get_pwd_index()];
-                g_data.env_list[get_pwd_index()] = ft_strjoin(ft_strdup("PWD="), s);
-                free(item);
-                // free(g_data.env_list[get_pwd_index()]);
-                // printf("cwd after == %s\n",s);
-                // free(tmp);
-
-            }
-            // free(tmp);
-        }
+            change_dir(cmd_strc, s, str_join, pwd);
     }
     free (s);
 
