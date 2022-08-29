27560ed88291acab17818a28c4d5efe30ddbf53a
Ahmed Ez zaouia <ahmez-za@e1r7p3.1337.ma>
2022-08-29 17:06:23 +0100

    remove leaks on cd alone

--- execution/ft_cd_cmd_utils.c
+++ execution/ft_cd_cmd_utils.c
@@ -6,7 +6,7 @@
 /*   By: ahmez-za <ahmez-za@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/29 00:34:27 by ahmez-za          #+#    #+#             */
-/*   Updated: 2022/08/29 00:35:56 by ahmez-za         ###   ########.fr       */
+/*   Updated: 2022/08/29 16:58:37 by ahmez-za         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,19 @@ int	get_oldpwd_index(void)
 void	go_to_home(char *s)
 {
 	char	*item;
+	char	*home;
 
 	item = NULL;
-	if (chdir(ft_get_env("HOME")) == -1)
+	home = ft_get_env("HOME");
+	if (chdir(home) == -1)
 		perror("Minishell :");
 	else
 	{
-		item = g_data.env_list[get_pwd_index()];
 		s = getcwd(NULL, 0);
+		item = g_data.env_list[get_pwd_index()];
 		g_data.env_list[get_pwd_index()] = ft_strjoin(ft_strdup("PWD="), s);
 		free(item);
 		free(s);
 	}
+	free(home);
 }
